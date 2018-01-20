1c902e685b281cb2382f12ed491e85c84c2fe782
deneeeeeeb <deneeeeeeb@gmail.com>
2018-01-20 23:57:50 +0900

    add execute permission

--- bin/rundaemon.sh
+++ bin/rundaemon.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-start-stop-daemon --start --background --exec "python /home/pi/discordbot/weaponbot.py" --make-pidfile --pidfile=/tmp/weaponbot.pid
-start-stop-daemon --start --background --exec "python /home/pi/discordbot/teambot.py" --make-pidfile --pidfile=/tmp/teambot.pid
+start-stop-daemon --start --background --exec /home/pi/discordbot/weaponbot.py --make-pidfile --pidfile=/tmp/weaponbot.pid
+start-stop-daemon --start --background --exec /home/pi/discordbot/teambot.py --make-pidfile --pidfile=/tmp/teambot.pid
