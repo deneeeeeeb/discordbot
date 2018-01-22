09ec4f9576143340484ca4670636d59f8f610387
deneeeeeeb <deneeeeeeb@gmail.com>
2018-01-22 21:02:06 +0900

    add slackbot

--- bin/killdaemon.sh
+++ bin/killdaemon.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 start-stop-daemon --stop --pidfile /tmp/weaponbot.pid
 start-stop-daemon --stop --pidfile /tmp/teambot.pid
+start-stop-daemon --stop --pidfile /tmp/googlehomebot.pid
--- bin/rundaemon.sh
+++ bin/rundaemon.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 start-stop-daemon --start --background --exec /home/pi/discordbot/weaponbot.py --make-pidfile --pidfile=/tmp/weaponbot.pid
 start-stop-daemon --start --background --exec /home/pi/discordbot/teambot.py --make-pidfile --pidfile=/tmp/teambot.pid
+start-stop-daemon --start --background --exec /home/pi/slackbot/run.py --make-pidfile --pidfile=/tmp/googlehomebot.pid
