d03372973502ae9a6f92a8f02a528aedd601ef5c
deneeeeeeb <deneeeeeeb@gmail.com>
2018-01-24 21:35:52 +0900

    add tweetsync runner

--- bin/killdaemon.sh
+++ bin/killdaemon.sh
@@ -2,3 +2,4 @@
 start-stop-daemon --stop --pidfile /tmp/weaponbot.pid
 start-stop-daemon --stop --pidfile /tmp/teambot.pid
 start-stop-daemon --stop --pidfile /tmp/googlehomebot.pid
+start-stop-daemon --stop --pidfile /tmp/tweetsync.pid
--- bin/rundaemon.sh
+++ bin/rundaemon.sh
@@ -2,3 +2,4 @@
 start-stop-daemon --start --background --exec /home/pi/discordbot/weaponbot.py --make-pidfile --pidfile=/tmp/weaponbot.pid
 start-stop-daemon --start --background --exec /home/pi/discordbot/teambot.py --make-pidfile --pidfile=/tmp/teambot.pid
 start-stop-daemon --start --background --exec /home/pi/slackbot/run.py --make-pidfile --pidfile=/tmp/googlehomebot.pid
+start-stop-daemon --start --background --exec /home/pi/tweetsync/post_discord.py --make-pidfile --pidfile=/tmp/tweetsync.pid
